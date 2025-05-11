2e1bff9b70089e23593bc3c220962e4005c50d9b
Shao <779224180@qq.com>
2025-05-11 15:42:32 +0800

    fix convert_png

--- aast.py
+++ aast.py
@@ -17,17 +17,18 @@ def ast_to_png(ast: AST) -> str:
         for child in node.children:
             child_id = str(id(child))
             if isinstance(child.symbol, TerminalSymbol):
-                child_name = TerminalTable[child.symbol.symbol_id]
+                child_name = child.val.value()
             elif isinstance(child.symbol, NonTerminalSymbol):
                 child_name = NonTerminalTable[child.symbol.symbol_id]
             else:
                 raise RuntimeError(f'Unknown symbol type: {child.symbol}')
 
             graph.node(child_id, child_name)
-            graph.edge(child_id, node_id, child_name)
+            graph.edge(node_id, child_id)
             add_nodes_edges(graph, child)
 
     dot = Digraph(comment='二叉树可视化')
+    dot.node(str(id(ast.root)), NonTerminalTable[ast.root.symbol.symbol_id])
     add_nodes_edges(dot, ast.root)
 
     output_dir = os.path.join(os.path.curdir, 'output')
@@ -47,9 +48,10 @@ if __name__ == "__main__":
     #                      1   *
     #                         / \
     #                        2   3
+    node4 = ASTNode(TerminalSymbol(TerminalTable.index('*')), None, Token(TT_NUMBER, '*'))
     node3 = ASTNode(TerminalSymbol(TerminalTable.index('NUM')), None, Token(TT_NUMBER, '3'))
     node2 = ASTNode(TerminalSymbol(TerminalTable.index('NUM')), None, Token(TT_NUMBER, '2'))
-    node_mul = ASTNode(NonTerminalSymbol(NonTerminalTable.index('Factor')), [node2, node3], None)
+    node_mul = ASTNode(NonTerminalSymbol(NonTerminalTable.index('Factor')), [node2, node4, node3], None)
     node1 = ASTNode(TerminalSymbol(TerminalTable.index('NUM')), [], Token(TT_NUMBER, '1'))
     root_node = ASTNode(NonTerminalSymbol(NonTerminalTable.index('Element')), [node1, node_mul], None)
 
